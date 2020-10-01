d3082d36851fe18b1b6ffea8698b7b9e68ba4bbe
Kellina Oliveira <kellina@gmail.com>
2020-09-30 22:10:40 -0300

    swagger add

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 const express = require('express') // importa modulo que retorna uma f. construtora
 const debug = require('debug')('server')
-const todoRouter = require('./todo/routers')
 const bodyParser = require('body-parser')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const todoRouter = require('./todo/routers')
 
 const app = new express()
 app.use(bodyParser.json()) //habiliata express entender o json
 
 todoRouter(app)
 
-app.get('/', (req, res) => {
-    res.send('Hello World!') // servidor funcionando
-})
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(8080, () => {
         debug('Pronto para receber conexões na porta 8080.')
