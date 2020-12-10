8c3d7ec2e63521115c002f620a5020ed05fe31fa
Kellina <kellina@gmail.com>
2020-12-10 18:44:04 -0300

    organizacao do app.use

--- src/server.js
+++ src/server.js
@@ -1,17 +1,16 @@
 const express = require('express') // importa modulo que retorna uma f. construtora
-const debug = require('debug')('server')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const todoRouter = require('./todo/routers')
 
-const app = new express()
-app.use(bodyParser.json()) //habiliata express entender o json
-app.use(cors())
+const app = express()
+app.use(bodyParser.json()) //habiliata express entender o json do body da requisição (qdo tiver)
+    .use(cors()) //permite que recursos restritos em uma página da web sejam recuperados por outro domínio de fora ao qual pertence o recurso que será recuperado.
+    .use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 todoRouter(app)
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app
