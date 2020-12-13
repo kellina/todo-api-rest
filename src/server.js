15e150c167c90c6d4bc5b7e96b4daa5188b4d6d8
Kellina <kellina@gmail.com>
2020-12-13 15:02:57 -0300

    todo router fixed

--- src/server.js
+++ src/server.js
@@ -6,11 +6,11 @@ const swaggerDocument = require('./swagger.json');
 const todoRouter = require('./todo/routers')
 
 const app = express()
-app.use(bodyParser.json()) //habiliata express entender o json do body da requisição (qdo tiver)
-    .use(cors()) //permite que recursos restritos em uma página da web sejam recuperados por outro domínio de fora ao qual pertence o recurso que será recuperado.
-    .use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 todoRouter(app)
 
+app.use(bodyParser.json()) //habiliata express entender o json do body da requisição (qdo tiver)
+    .use(cors()) //permite que recursos restritos em uma página da web sejam recuperados por outro domínio de fora ao qual pertence o recurso que será recuperado.
+    .use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app
